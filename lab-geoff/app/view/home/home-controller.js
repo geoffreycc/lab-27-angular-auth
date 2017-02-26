f0947552b9a0326e3b60d3da5a98da839c6c0c5e
Geoffrey Cox <gcox3d@gmail.com>
2017-02-26 12:02:17 -0800

    added a logout button to home page

--- lab-geoff/app/view/home/home-controller.js
+++ lab-geoff/app/view/home/home-controller.js
@@ -1,7 +1,15 @@
 require('./home.scss');
 
-module.exports = ['$log', HomeController];
+module.exports = ['$log', '$location', 'authService', HomeController];
 
-function HomeController($log) {
+function HomeController($log, $location, authService) {
   $log.debug('HomeController');
+
+  this.logOut = function() {
+    $log.debug('homeCtrl.logOut()');
+
+    authService.logout();
+    //redirect to login
+    $location.url('/login');
+  };
 }
