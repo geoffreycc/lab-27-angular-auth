afd164c2117d56152fc9c2a2281723a89024f917
Geoffrey Cox <gcox3d@gmail.com>
2017-02-26 07:07:00 -0800

    updated signup.js

--- lab-geoff/app/component/landing/signup/signup.js
+++ lab-geoff/app/component/landing/signup/signup.js
@@ -1,16 +1,23 @@
 module.exports = {
   template: require('./signup.html'),
   controller: ['$log', '$location', 'authService', SignupController],
+  controllerAs: 'signupCtrl'
 };
 
 function SignupController($log, $location, authService) {
   $log.debug('SignupController');
 
-  //get token
+  authService.getToken()
+  .then( () => {
+    $location.url('/home');
+  });
 
   this.signup = function(user) {
     $log.debug('signupCtrl.signup');
 
-    //signup
+    authService.signup(user)
+    .then( () => {
+      $location.url('/home');
+    });
   };
 }
