81d66950a04da8d31bb0ce8971e636bec7f8ff63
Geoffrey Cox <gcox3d@gmail.com>
2017-02-26 07:36:00 -0800

    added auth-service

---
+++ lab-geoff/app/service/auth-service.js
@@ -0,0 +1,87 @@
+module.exports = ['$q', '$log', '$http', '$window', authService];
+
+function authService($q, $log, $http, $window) {
+  $log.debug('authService');
+
+  let service = {};
+  let token = null;
+
+  function setToken(_token) {
+    $log.debug('authService.setToken()');
+
+    if (!_token) {
+      return $q.reject(new Error('no token'));
+    }
+
+    $window.localStorage.setItem('token', _token);
+    token = _token;
+    return $q.resolve(token);
+  }
+
+  service.getToken = function() {
+    $log.debug('service.getToken()');
+
+    if (token) {
+      return $q.resolve(token);
+    }
+    token = $window.localStorage.getItem('token');
+
+    if (token) return $q.resolve(token);
+    return $q.reject(new Error('no token found'));
+  };
+
+  service.logout = function() {
+    $log.debug('authService.logout()');
+
+    $window.localStorage.removeItem('token');
+    token = null;
+    return $q.resolve();
+  };
+
+  service.login = function(user) {
+    $log.debug('authService.login()');
+
+    let url = `${__API_URL__}/api/signup`;
+    let base64 = $window.btoa(`${user.username}:${user.password}`);
+    let config = {
+      headers: {
+        'Authorization': `Basic ${base64}`,
+        'Accept': 'application/json',
+      }
+    };
+
+    return $http.get(url, config)
+    .then(res => {
+      $log.log('Success', res.data);
+      return setToken(res.data);
+    })
+    .catch(err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
+  service.signup = function(user) {
+    $log.debug('authService.signup()');
+
+    let url = `${__API_URL__}/api/signup`;
+    let config = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      }
+    };
+
+    return $http.post(user, url, config)
+      .then(res => {
+        $log.log('Success', res.data);
+        return setToken(res.data);
+      })
+      .catch(err => {
+        $log.error('Failure', err.message);
+        return $q.reject(err);
+      });
+  };
+
+  return service;
+}
