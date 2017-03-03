516d74d4d61daa9ffe71acacc8c66e24461ca954
Geoffrey Cox <gcox3d@gmail.com>
2017-03-02 16:20:46 -0800

    gallery component template and create gallery method

---
+++ lab-geoff/app/component/gallery/gallery.js
@@ -0,0 +1,21 @@
+require('./gallery.scss');
+
+module.exports = {
+  template: require('./gallery.html'),
+  controller: ['$log', 'galleryService', GalleryController],
+  controllerAs: 'galleryCtrl'
+};
+
+function GalleryController ($log, galleryService) {
+  $log.debug('GalleryController');
+
+  this.gallery = {};
+
+  this.create = function() {
+    galleryService.createGallery(this.gallery)
+    .then( () => {
+      this.gallery.galleryName = null;
+      this.gallery.galleryDesc = null;
+    });
+  };
+}
